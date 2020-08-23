17f95d1cef316e9082038f2cb1b5abd2ec0e78d4
James Barrett <45885505+JNBarr@users.noreply.github.com>
2020-08-23 18:00:31 +0100

    Add files via upload
    First commit, will keep this repo up if i can.

---
+++ testing.java
@@ -0,0 +1,50 @@
+// all in one .java file
+// on an online editor
+// a real project would be
+// in eclipse or another IDE.
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    int mytestint; 
+    //mytestint = mytestint + 1; (incorrect)
+    mytestint = 1;
+    System.out.print(mytestint);
+    short shortnumber;
+    shortnumber = 1000;
+    System.out.print(shortnumber);
+  }
+}
+// testing  comments
+class PrintOut {
+  public static void main(String[] args) {
+    System.out.println("Test 2nd");  }
+    //class doesnt print
+}
+
+// testing the class doesnt explode. 
+class DontErrorMe {
+  public static void main(String[] args) {
+    System.out.println("Test Again."); }
+  }
+
+class veryclassy {
+  public static void main(String[] args) {
+    System.out.println("why public");
+  }
+}
+
+// spacing
+/*
+public class exampleclass {
+  int e = 2;
+   public static void main(String[] args) {
+    MyClass myObj = new MyClass();
+    System.out.println(myObj.x);
+}
+*/
+//spacing
+/*
+public class anotherclass {
+  int b = 3;
+}
+*/
