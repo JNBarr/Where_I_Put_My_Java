37b5ef8c8ea5118fab91fa1d21728fa2733d2fd3
James Barrett <45885505+JNBarr@users.noreply.github.com>
2020-08-30 19:21:50 +0100

    Add files via upload
    part 5 mostly bool stuff

---
+++ testing_part5.java
@@ -0,0 +1,44 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    boolean light = true; 
+    boolean dark = false;
+
+    System.out.println(light);
+    System.out.println(dark);
+
+    // boolean expressions
+    /* w3 code
+    int x = 10;
+    int y = 9;
+    System.out.println(x > y); // returns true, because 10 is higher than 9
+    */
+
+    // boolean expression
+    int f = 5;
+    int e = 6;
+    System.out.println(f > e);
+    //boolean checkit = ("need some ifs here");
+    // complie error, string instead of bool
+
+    /* not java selection
+    if e = 6 {
+      system.out.println("Bingo");
+    }
+    */
+
+    System.out.println(10 > 11);
+    /////
+    int isthenumber = 101;
+    System.out.println(isthenumber == 0);
+    System.out.println(isthenumber == 101);
+
+    // just a test here for var = value or str
+    System.out.println(light == true);
+    System.out.println(dark == false);
+
+
+
+  }
+}
