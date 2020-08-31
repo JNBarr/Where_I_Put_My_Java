8687b1c53289584cded42f5086866f0eb0b411b1
James Barrett <45885505+JNBarr@users.noreply.github.com>
2020-08-31 17:11:33 +0100

    Add files via upload
    selection details: can work within method but couldn't call.

---
+++ test_part6.java
@@ -0,0 +1,73 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+    public void MyMethod()
+    {
+      System.out.println("Hello world!");
+      int bbb = 45;
+      if (bbb == 45) {
+        System.out.println("True.");
+      }
+      else {
+        System.out.println("Error.");
+      }
+      // okay so the if statements work in method
+    }
+   // System.out.println(MyMethod());
+  // if statements
+  // simple condtions first
+
+  /*
+  if (1 < 2) {
+    System.out.println("One is less than two.");
+  }
+  */
+  // still doesnt work.
+  /*
+  int please = 10;
+  int work = 20;
+  if (please > work) {
+    System.out.println("condtion is true.");
+  }
+  */
+
+/*
+int bbb = 45;
+
+if (bbb == 45) {
+  System.out.println("True.");
+}
+else {
+  System.out.println("Error.");
+  }
+}
+*/
+
+} // end of main class
+
+class TryMyMetho {
+  // Main.MyMethod();
+  // cant call the method but can declare
+  // the if statement within it. 
+}
+
+// trying the if statements
+// outside of the main public class.
+/* still throwing errors
+// class expected.
+if (3 > 2) {
+  System.out.println("three is more than two. ");
+} 
+*/
+/*
+class Selection {
+  public static void main(String[] args) {
+    if (3 > 2) {
+  System.out.println("three is more than two.");
+    } 
+  }
+}
+*/
+// no errors thrown from the above code. 
+// but no output.
