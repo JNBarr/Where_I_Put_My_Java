9fce3aff9e322a49e800ed20d2b96c4ad5c6262c
James Barrett <45885505+JNBarr@users.noreply.github.com>
2020-09-13 14:21:52 +0100

    Add files via upload
    Do whiles

---
+++ testing_part8.java
@@ -0,0 +1,37 @@
+class Main {
+  public static void main(String[] args) {
+
+    System.out.println("Hello world!");
+
+    int i = 0;
+      do {
+          System.out.println(i);
+          i++; // Incriment.
+        }
+      while (i < 5);  
+    // Will print 0 - 4.
+    // Attempting my own one here.
+    int b = 1;
+      do { //start do
+        System.out.println(b);
+        b++;
+      } // finish do.
+    while (b < 10);
+
+  
+  } // end of main method. 
+
+
+} // end of class. use comments to keep track.
+/*
+public class MyClass {
+  public static void main(String[] args) {
+    int i = 0;
+    do {
+      System.out.println(i);
+      i++;
+    }
+    while (i < 5);  
+  }
+}
+*/
